b0cb0733da303587a56825ad77a1b8c34cb2d777
Glitch (dashboard-prototype) <none>
2019-09-03 07:10:45 +0000

    ⏰🚥 Updated with Glitch

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+$.ajax({
+  url: "https://spreadsheets.google.com/feeds/list/1vzPjhUkw91WcOVjddorMeoKQ7yGT570tK4Cd_0XFcwA/od6/public/values?alt=json",
+  data: {
+    zipcode: 97201
+  },
+  success: function( result ) {
+    var rows = result['feed']['entry'];
+    var $div = $('.main-overview').addClass('');
+  
+    rows.forEach(function(row) {
+    var trend = row['gsx$trendtitle']['$t']; // row.value
+    var imgurl = row['gsx$imageurl']['$t'];
+    var description = row['gsx$description']['$t'];
+      $div.append('<div class="flip">'+ '<div class="front">'+'<div class="imagecard">' + '<img src ="' + imgurl + '"class="gallery__img">'
+                  + '' + '<div class="titlecard">' + trend + '</div></div></div>' + '	<div class="back">' + '<h3>' + description + '</h3>' + '</div></div>')
+  });
+  }
+});
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+$(function(){
+
+    $(".flip").flip({
+        axis: "x", // y or x
+        reverse: true, // true and false
+        trigger: "hover", // click, hover or manual
+        speed: 500
+    });
+
+    setTimeout(function() {
+        $('.flip').flip(true);
+    },3000);
+});
+
